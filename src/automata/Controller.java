e274245126c5e2d570baf02ceda454881f100d97
Nikita Kuchur <nikitakuchur@gmail.com>
2018-12-31 02:16:52 +0300

    Updated controller

--- src/automata/Controller.java
+++ src/automata/Controller.java
@@ -4,9 +4,11 @@ import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
@@ -53,6 +55,23 @@ public class Controller implements Initializable {
             draw(canvas.getGraphicsContext2D());
         });
 
+        canvas.setOnMouseDragged(event -> {
+            Point2D boardPosition = getBoardPosition();
+
+            int x = (int) ((event.getX() - boardPosition.getX()) /
+                          ((canvas.getWidth() - 2 * boardPosition.getX()) / board.getWidth()));
+
+            int y = (int) ((event.getY() - boardPosition.getY()) /
+                          ((canvas.getHeight() - 2 * boardPosition.getY()) / board.getHeight()));
+
+            if (event.getButton() == MouseButton.PRIMARY)
+                board.revive(x, y);
+            else if (event.getButton() == MouseButton.SECONDARY)
+                board.kill(x, y);
+
+            draw(canvas.getGraphicsContext2D());
+        });
+
         service = new Service<Void>() {
             @Override
             protected Task<Void> createTask() {
@@ -89,32 +108,44 @@ public class Controller implements Initializable {
         });
     }
 
+    private double getCellSize() {
+        double wr = canvas.getWidth() / board.getWidth();
+        double hr = canvas.getHeight() / board.getHeight();
+
+        return wr > hr ? hr : wr;
+    }
+
+    private Point2D getBoardPosition() {
+        double size = getCellSize();
+        double x = canvas.getWidth() / 2 - board.getWidth() * size / 2;
+        double y = canvas.getHeight() / 2 - board.getHeight() * size  / 2;
+
+        return new Point2D(x, y);
+    }
+
     // TODO: Fix it
     private void draw(GraphicsContext gc) {
-        double size = canvas.getWidth() / board.getWidth();
-
-        if (board.getHeight() * size > canvas.getHeight())
-            size = canvas.getHeight() / board.getHeight();
+        double size = getCellSize();
+        Point2D boardPosition = getBoardPosition();
 
         gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-
-        double xPos = canvas.getWidth() / 2 - board.getWidth() * size / 2;
-        double yPos = canvas.getHeight() / 2 - board.getHeight() * size  / 2;
 
         // Draw the grid
         gc.setStroke(Color.LIGHTGRAY);
         for (int i = 0; i <= board.getWidth(); i++)
-            gc.strokeLine(xPos + i * size, yPos, xPos + i * size, yPos + board.getHeight() * size);
+            gc.strokeLine(boardPosition.getX() + i * size, boardPosition.getY(),
+                          boardPosition.getX() + i * size, boardPosition.getY() + board.getHeight() * size);
 
         for (int i = 0; i <= board.getHeight(); i++)
-            gc.strokeLine(xPos, yPos + i * size, xPos + board.getWidth() * size, yPos + i * size);
+            gc.strokeLine(boardPosition.getX(), boardPosition.getY() + i * size,
+                      boardPosition.getX() + board.getWidth() * size, boardPosition.getY() + i * size);
 
         // Draw cells
         gc.setFill(Color.ORANGE);
         for (int i = 0; i < board.getWidth(); i++)
             for (int j = 0; j < board.getHeight(); j++)
                 if (board.isAlive(i, j))
-                    gc.fillRect(xPos + i * size, yPos + j * size, size, size);
+                    gc.fillRect(boardPosition.getX() + i * size, boardPosition.getY() + j * size, size, size);
     }
 
     @FXML
@@ -123,11 +154,13 @@ public class Controller implements Initializable {
         draw(canvas.getGraphicsContext2D());
     }
 
+    @FXML
     public void handleClearButtonClick() {
         board.clear();
         draw(canvas.getGraphicsContext2D());
     }
 
+    @FXML
     public void handlePlayButtonClick() {
         service.start();
     }
